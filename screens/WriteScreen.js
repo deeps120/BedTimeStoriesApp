adcd690e69e0217fde3305bcbce66c30b976f4c4
deeps120 <62687789+deeps120@users.noreply.github.com>
2020-08-08 14:33:51 +0530

    Add files via upload

---
+++ screens/WriteScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+ class WriteScreen extends React.Component {
+    render(){
+  return (
+      <Text>write stories!</Text>
+  );
+    }
+}
+export default WriteScreen;
